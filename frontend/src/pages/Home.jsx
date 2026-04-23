5c03dd2b29e7fdbb352ed51802c7629190c8b4ea
sadiya-khan1857 <sadiyashoaib1@gmail.com>
2026-04-23 16:06:49 +0530

    Fix Home export issue and installed tailwind

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl text-blue-500">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
