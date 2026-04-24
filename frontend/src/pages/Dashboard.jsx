58c3cbcf37d30d26d4cb4f8ad5bd56520e286360
sadiya-khan1857 <sadiyashoaib1@gmail.com>
2026-04-24 11:40:35 +0530

    Enhance dashboard title (#2)

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
   return (
     <div className="p-6 max-w-5xl mx-auto">
       <h1 className="text-3xl font-bold mb-6 text-center">
-       AI Code Review System
+       Smart AI Code Review Syste
       </h1>
 
       {/* Code Input */}
